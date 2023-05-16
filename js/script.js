601df2791dce383d49b83a2f5a3ce3ad9902fe43
Shanae Brooks <124213751+Shanae-B@users.noreply.github.com>
2023-05-16 19:51:16 -0400

    Added global variables

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Global Variables
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess button");
+const text = document.querySelector.Selector(".text input");
+const mysteryWord = document.querySelector(".word-in-progress");
+const remaining = document.querySelector (".remaining");
+const guesses = document.querySelector("span");
+//console.log(guesses)
+const message = document.querySelector(".message");
+const hiddenButton = document.querySelector(".play-again hide");
+const word = "magnolia"; 
