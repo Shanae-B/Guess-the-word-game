d8739c266743ae53595274cfcc664175421b8dc3
Shanae Brooks <124213751+Shanae-B@users.noreply.github.com>
2023-05-17 16:04:18 -0400

    Corrected syntax error

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Global Variables
 const guessedLetters = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
-const textInput = document.querySelector.Selector(".letter");
+const textInput = document.querySelector(".letter");
 const mysteryWord = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector (".remaining");
 const guessesSpan = document.querySelector(".remaining span");
