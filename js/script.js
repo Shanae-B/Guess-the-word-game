e027b48954fa9415e16107390ddb67ae2acd6a0f
Shanae Brooks <124213751+Shanae-B@users.noreply.github.com>
2023-05-23 18:18:45 -0400

    Created function for word in progress

--- js/script.js
+++ js/script.js
@@ -3,44 +3,63 @@ const guessedLetters = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const textInput = document.querySelector(".letter");
 const mysteryWord = document.querySelector(".word-in-progress");
-const remainingGuesses = document.querySelector (".remaining");
+const remainingGuesses = document.querySelector(".remaining");
 const guessesSpan = document.querySelector(".remaining span");
 
 const message = document.querySelector(".message");
 const hiddenButton = document.querySelector(".play-again");
-const word = "magnolia"; 
+const word = "magnolia";
 
 //Placeholders for each letter
-const placeHolder = function(word){
+const placeHolder = function (word) {
     const placeHolderLetters = [];
     for (const letter of word) {
         console.log(letter);
-        placeHolderLetters.push(" ● ");
+        placeHolderLetters.push("●");
     }
     mysteryWord.innerText = placeHolderLtters.join("");
 };
 
 placeHolder(word);
 
-guessButton.addEventListener("click", function (e){
+guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const ouputValue = textInput.value;
-    console.log(outputValue);
+    //console.log(outputValue);
+    const validGuess = validInput(guess);
+
+    if (validGuess) {
+
+        makeGuess(guess);
+
+    }
+
     textInput.value = "";
+});
 
-}); 
-
-input = function(){
+validInput = function (input) {
     const acceptedLetter = /[a-zA-Z]/
-    const guessInput = acceptedLetter.match("")
 
-    if (guessInput) {
-        console.log("Enter a letter")
+    if (input.length === 0) {
+
+        message.innerText = "Please enter a letter.";
+
+    } else if (input.length > 1) {
+
+        message.innerText = "Please enter one letter";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z";
+    } else {
+        return input;
     }
 
-    if (input !== /[a-zA-Z]/) {
-        alert("Enter 1 letter at a time")
-    }
+const wordInProgress = function (guessedLetters) {
+wordInProgress(guessedLetters);
+}
 
+const wordUpper = word.toUpperCase();
+const wordArray = wordUpper.split("");
+return wordArray;
 
 }
